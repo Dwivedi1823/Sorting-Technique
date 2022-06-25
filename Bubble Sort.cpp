70dd8301f94e1514452c5d162095175fdd5b8f68
Piyush Kumar <64326511+Dwivedi1823@users.noreply.github.com>
2022-06-25 17:18:47 +0530

    Update Bubble Sort.cpp

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -13,17 +13,14 @@ int main()
     
     int cnt = 1;
     
-    while(cnt < n)
-    {
-        for(int  i = 0; i < n - cnt; i++)
-        {
+    while(cnt < n){
+        for(int  i = 0; i < n - cnt; i++){
             if(arr[i] > arr[i+1])
                 swap(arr[i],arr[i+1]);
         }
         cnt++;
     }
-    for(int  i = 0; i < n; i ++)
-    {
+    for(int  i = 0; i < n; i ++){
         cout<<arr[i]<<" ";
     }
     
